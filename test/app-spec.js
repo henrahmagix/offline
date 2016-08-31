cd54d907b70b28b4936cc253bb6a81c2f17b69f4
Henry Blyth <blyth.henry@gmail.com>
2016-08-31 22:45:41 +0100

    Setup Karma tests

---
+++ test/app-spec.js
@@ -0,0 +1,13 @@
+describe('the suite', function () {
+
+    beforeEach(function () {
+
+        module('app');
+
+    });
+
+    it('should foo', function () {
+        expect(true).toBe(true);
+    });
+
+});
